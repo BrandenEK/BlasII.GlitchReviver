3315907e21ce25a9123f14a166d3499c239ea038
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2024-12-07 18:49:09 -0500

    init: add base project files

---
+++ BlasII.GlitchReviver/Main.cs
@@ -0,0 +1,13 @@
+﻿using MelonLoader;
+
+namespace BlasII.GlitchReviver;
+
+internal class Main : MelonMod
+{
+    public static GlitchReviver GlitchReviver { get; private set; }
+
+    public override void OnLateInitializeMelon()
+    {
+        GlitchReviver = new GlitchReviver();
+    }
+}
---
+++ BlasII.GlitchReviver/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+using BlasII.GlitchReviver;
+using MelonLoader;
+
+[assembly: MelonInfo(typeof(BlasII.GlitchReviver.Main), ModInfo.MOD_NAME, ModInfo.MOD_VERSION, ModInfo.MOD_AUTHOR)]
+[assembly: MelonOptionalDependencies("Modding API", "0.1.0")]
