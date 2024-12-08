f2bf5364dd299ba4d4f44a16fe71a7a336565d06
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2024-12-07 19:24:49 -0500

    core: add keybindings and input processing

--- BlasII.GlitchReviver/GlitchConfig.cs
+++ BlasII.GlitchReviver/GlitchConfig.cs
@@ -6,5 +6,13 @@ namespace BlasII.GlitchReviver;
 /// </summary>
 public class GlitchConfig
 {
+    /// <summary>
+    /// Whether ruego/mc dives can be activated by canceling mirabras
+    /// </summary>
+    public bool AllowMirabrasDive { get; set; }
 
+    /// <summary>
+    /// Whether mc projectiles can be spawned by swapping directions
+    /// </summary>
+    public bool AllowMeaCulpaHover { get; set; }
 }
