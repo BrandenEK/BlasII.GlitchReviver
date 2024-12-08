707390263e0a8fbbf7539be6195d68b788a131ef
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2024-12-07 22:17:54 -0500

    mirabras: finalize code

--- BlasII.GlitchReviver/MirabrasDive/Patches.cs
+++ BlasII.GlitchReviver/MirabrasDive/Patches.cs
@@ -4,10 +4,7 @@ using HarmonyLib;
 using Il2CppLightbug.Kinematic2D.Implementation;
 using Il2CppTGK.Game;
 using Il2CppTGK.Game.Components.Abilities;
-using Il2CppTGK.Game.Components.Abilities.Inputs;
-using Il2CppTGK.Game.Components.Attack.Data;
 using Il2CppTGK.Game.Components.Prayers;
-using UnityEngine;
 
 namespace BlasII.GlitchReviver.MirabrasDive;
 
@@ -19,15 +16,18 @@ class ChangeWeaponAbility_OnUpdate_Patch
         if (!Main.GlitchReviver.CurrentConfig.AllowMirabrasDive)
             return;
 
-        //if (!Main.GlitchReviver.InputHandler.GetButtonDown(ModdingAPI.Input.ButtonType.NextWeapon))
-        //    return;
-        if (!Input.GetKeyDown(KeyCode.R))
+        if (__instance.GetState() != AbilityState.EXECUTING)
+            return;
+
+        if (!Main.GlitchReviver.InputHandler.GetButtonDown(ModdingAPI.Input.ButtonType.NextWeapon))
             return;
 
         if (CoreCache.EquipmentManager.CountUnlockedWeapons() < 2)
             return;
 
+#if DEBUG
         ModLog.Info("Activating module 'MirabrasDive'");
+#endif
 
         var changeWeapon = AssetStorage.Abilities[ABILITY_IDS.ChangeWeapon];
         var fullPrayer = AssetStorage.Abilities[ABILITY_IDS.FullPrayer];
@@ -36,68 +36,6 @@ class ChangeWeaponAbility_OnUpdate_Patch
         controller.CancelAbility(fullPrayer);
         controller.ActivateAbilityByType(changeWeapon);
 
-        WeaponID currentWeapon = CoreCache.EquipmentManager.GetCurrentWeapon();
-        int nextWeaponSlot = CoreCache.EquipmentManager.GetWeaponSlot(currentWeapon) + 1;
-        if (nextWeaponSlot >= CoreCache.EquipmentManager.GetNumWeaponSlots())
-            nextWeaponSlot = 0;
-        WeaponID nextWeapon = CoreCache.EquipmentManager.GetAssignedWeaponToSlot(nextWeaponSlot);
-
-        //__instance.ChangeWeapon(nextWeapon);
+        CoreCache.PlayerSpawn.PlayerControllerRef.GetAbility<ChangeWeaponAbility>().RequestFastChangeNextWeapon();
     }
 }
-
-[HarmonyPatch(typeof(ChangeWeaponAbility), nameof(ChangeWeaponAbility.OnIgnore))]
-class ChangeWeaponAbility_OnUpdate_Patch2
-{
-    public static void Postfix(ChangeWeaponAbility __instance)
-    {
-
-        ModLog.Warn("ignore");
-    }
-}
-
-[HarmonyPatch(typeof(ChangeWeaponInputsReaderWithMeaCulpa), nameof(ChangeWeaponInputsReaderWithMeaCulpa.ConsumeInput))]
-class t
-{
-    public static void Postfix()
-    {
-        ModLog.Warn("Consume");
-    }
-}
-
-[HarmonyPatch(typeof(ChangeWeaponInputsReaderWithMeaCulpa), nameof(ChangeWeaponInputsReaderWithMeaCulpa.ProcessCharacterActionsInfo))]
-class t2
-{
-    public static void Postfix()
-    {
-        //ModLog.Warn("process");
-    }
-}
-
-
-// These only work if not in the prayer, so I have to use the old method
-
-//[HarmonyPatch(typeof(ChangeWeaponAbility), nameof(ChangeWeaponAbility.RequestFastChange))]
-//class ChangeWeaponAbility_RequestFastChange_Patch
-//{
-//    public static void Postfix()
-//    {
-//        ModLog.Error("request weapon change");
-//    }
-//}
-//[HarmonyPatch(typeof(ChangeWeaponAbility), nameof(ChangeWeaponAbility.RequestFastChangeNextWeapon))]
-//class ChangeWeaponAbility_RequestFastChangeNextWeapon_Patch
-//{
-//    public static void Postfix()
-//    {
-//        ModLog.Error("request weapon change");
-//    }
-//}
-//[HarmonyPatch(typeof(ChangeWeaponAbility), nameof(ChangeWeaponAbility.RequestFastChangePrevWeapon))]
-//class ChangeWeaponAbility_RequestFastChangePrevWeapon_Patch
-//{
-//    public static void Postfix()
-//    {
-//        ModLog.Error("request weapon change");
-//    }
-//}
